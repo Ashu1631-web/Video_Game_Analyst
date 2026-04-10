5c3dcad53a229374257446efafccdc0c1f0dc815
Ashish Mehara <viratmehara232@gmail.com>
2026-04-10 21:04:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+# =========================================
+# 🎮 FINAL CLEAN STREAMLIT APP
+# =========================================
+
 import streamlit as st
 import pandas as pd
 import plotly.express as px
